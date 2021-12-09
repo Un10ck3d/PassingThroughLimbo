7702c0e9b7ddaa37ad599ba1e38620b5532cd77d
Jonathan Bangert <jonathan@bangert.dk>
2021-12-09 16:16:18 +0100

    FIXED COMPILE ERROR

--- PassingThroughLimbo/Assets/PlayerAssests/PlayerArmCodeing.cs
+++ PassingThroughLimbo/Assets/PlayerAssests/PlayerArmCodeing.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class PlayerArmCodeing : MonoBehaviour
 {
-    public Transform SwordTransform;
-    public Vector3 mouse_pos;
+    //public Transform SwordTransform;
+    //public Vector3 mouse_pos;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        Transform SwordTransform = GameObject.FindGameObjectWithTag("Sword").transform;
+        //Transform SwordTransform = GameObject.FindGameObjectWithTag("Sword").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        mouse_pos = Input.mousePosition;
-        angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
-        SwordTransform.rotation = Quaternion.Euler(Vector3(0, 0, angle));
+        //mouse_pos = Input.mousePosition;
+        //angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+        //SwordTransform.rotation = Quaternion.Euler(Vector3(0, 0, angle));
     }
 }
