9cc2e44c47b3ebf797b06e39b32154a63e450677
Jonathan Bangert <jonathan@bangert.dk>
2021-12-09 16:15:04 +0100

    Added some arm code i guess or something

---
+++ PassingThroughLimbo/Assets/PlayerAssests/PlayerArmCodeing.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerArmCodeing : MonoBehaviour
+{
+    public Transform SwordTransform;
+    public Vector3 mouse_pos;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform SwordTransform = GameObject.FindGameObjectWithTag("Sword").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouse_pos = Input.mousePosition;
+        angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+        SwordTransform.rotation = Quaternion.Euler(Vector3(0, 0, angle));
+    }
+}
